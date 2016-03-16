fc9c1131ad0457f225aa314bcba4a5f68e316dbc
MikeC1995 <mike_christensen@btinternet.com>
2016-03-16 21:26:50 +0000

    how it works steps implemented with steps and changing content view

--- app/app.js
+++ app/app.js
@@ -4,16 +4,43 @@
 var app = angular.module('app', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  /*$stateProvider
-    .state("home", {
-      url:"/",
+  /* When in steps state, root ui-view filled with another ui-view,
+  ** which is populated by the proper substate (i.e. a particular step)
+  */
+  $stateProvider
+    .state("steps", {
+      abstract: true,
       views: {
-        '': { templateUrl:"/app/components/map/map.view.html" }
+        '': { template: '<div ui-view></div>'}
       }
-    });*/
+    })
+    .state("steps.1", {
+      url: '/',
+      views: {
+        '': { templateUrl:"/app/components/how-it-works/step-1/step-1.html" }
+      },
+      data: {
+        step: 1
+      }
+    })
+    .state("steps.2", {
+      views: {
+        '': { templateUrl:"/app/components/how-it-works/step-2/step-2.html" }
+      },
+      data: {
+        step: 2
+      }
+    })
+    .state("steps.3", {
+      views: {
+        '': { templateUrl:"/app/components/how-it-works/step-3/step-3.html" }
+      },
+      data: {
+        step: 3
+      }
+    });
   $urlRouterProvider.otherwise("/");
 });
 
 app.controller('appController', function() {
-
 });
--- app/components/how-it-works/steps/steps.controller.js
+++ app/components/how-it-works/steps/steps.controller.js
@@ -3,4 +3,14 @@
 var app = angular.module('app');
 app.controller('stepsController', ['$scope', function($scope) {
 
+  $scope.currentStep = 1;
+
+  // this'll be called on every state change in the app
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if (angular.isDefined(toState.data.step)) {
+      $scope.currentStep = toState.data.step;
+      return;
+    }
+    $scope.currentStep = 1;
+  });
 }]);
